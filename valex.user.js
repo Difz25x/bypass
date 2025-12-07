3bdfec447f55fd7c641520e3cf813d3be5f97f39
Difz25x <177396246+Difz25x@users.noreply.github.com>
2025-12-08 05:44:39 +0700

    Update valex.user.js

--- valex.user.js
+++ valex.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Valex Bypass
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0.0
+// @version      1.0.1.0
 // @description  Bypass Volcano and Work.ink links with unified panel
 // @author       Difz25x
-// @match        https://key.volcano.wtf/*
-// @match        https://work.ink/*
+// @match        *://*.valex.io/*
+// @match        *://work.ink/3t0/*
 // @run-at       document-start
 // @grant        unsafeWindow
 // @downloadURL  https://github.com/Difz25x/bypass/raw/refs/heads/main/valex.user.js
