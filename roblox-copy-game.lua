ae9afe92fa0d09fc9702fa918d99cdc3ae567775
Difz25x <difz25x@gmail.com>
2025-12-28 14:47:44 +0700

    Create roblox-copy-game.lua

---
+++ roblox-copy-game.lua
@@ -0,0 +1,1 @@
+local Params={RepoURL="https://raw.githubusercontent.com/luau/SynSaveInstance/main/",SSI="saveinstance"} local s=loadstring(game:HttpGet(Params.RepoURL..Params.SSI..".luau"))() s({})
