532c4ea5c1ef2897027a8167b6574e3bb8c37ab2
Difz25x <difz25x@gmail.com>
2025-12-29 07:20:47 +0700

    Rename script and update version and URLs

---
+++ lockr.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Lockr Bypass
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0.0
+// @description  Bypass Lockr links
+// @author       Difz25x
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @downloadURL  https://raw.githubusercontent.com/Difz25x/bypass/refs/heads/main/lockr.user.js
+// @updateURL    https://raw.githubusercontent.com/Difz25x/bypass/refs/heads/main/lockr.user.js
+// @require      https://raw.githubusercontent.com/Difz25x/bypass/refs/heads/main/lockr-bypass.js
+// ==/UserScript==
