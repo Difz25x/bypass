978c6cd9a18087b752b7989481f20af6c2e1a1ec
Difz25x <difz25x@gmail.com>
2026-04-26 08:09:20 +0700

    Update workink.user.js

--- workink.user.js
+++ workink.user.js
@@ -1,10 +1,14 @@
 // ==UserScript==
 // @name         Work.ink Bypass
 // @namespace    http://tampermonkey.net/
-// @version      1.2.0.0
+// @version      1.2.1.0
 // @description  Bypass Work.ink links
 // @author       Difz25x
 // @match        https://work.ink/*
+// @match        https://*.work.ink/*
+// @match        https://workink.net/*
+// @match        https://*.workink.net/*
+// @match        https://yub-x.net/*
 // @run-at       document-start
 // @grant        unsafeWindow
 // @downloadURL  https://github.com/Difz25x/bypass/raw/refs/heads/main/workink.user.js
